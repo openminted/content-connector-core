2f6a6c282d6547dd7954081d506ac1fd97affd8e
John Diplas <Jdiplas@gmail.com>
2018-03-05 12:40:30 +0200

    Changed component paths that are getting scanned -REmoved mock connectors from connector-service

--- src/main/java/eu/openminted/content/connector/core/COREConnectorConfiguration.java
+++ src/main/java/eu/openminted/content/connector/core/COREConnectorConfiguration.java
@@ -3,14 +3,13 @@ package eu.openminted.content.connector.core;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  *
  * @author lucasanastasiou
  */
 @Configuration
-@ComponentScan("eu.openminted.content")
+@ComponentScan("eu.openminted.content.connector")
 //@PropertySource(value = {"classpath:application.properties"})
 public class COREConnectorConfiguration {
 
